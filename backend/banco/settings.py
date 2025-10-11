32a6dfaec68fe85d6cf1c0b7969f8aed0dcc8ccf
sebitabravo <sebastianalejandrobravocampos@gmail.com>
2025-10-11 18:17:23 -0300

    fix: aumentar MAX_PAGE_SIZE a 10000 para permitir obtener todos los clientes

--- backend/banco/settings.py
+++ backend/banco/settings.py
@@ -79,7 +79,7 @@ REST_FRAMEWORK = {
     },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
-    'MAX_PAGE_SIZE': 100,
+    'MAX_PAGE_SIZE': 10000,  # Aumentado para permitir obtener todos los clientes
 }
 
 
